7501f3f37e298d9c0b8aa38f6c2a5af0d429c699
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-11-16 10:29:31 -0800

    First Commit

---
+++ Project2/functions.cpp
@@ -0,0 +1,8 @@
+//
+//  functions.cpp
+//  Project2
+//
+//  Created by Chatchapong Thuaypha on 11/16/24.
+//
+
+#include <stdio.h>
---
+++ Project2/header.h
@@ -0,0 +1,12 @@
+//
+//  header.h
+//  Project2
+//
+//  Created by Chatchapong Thuaypha on 11/16/24.
+//
+
+#ifndef header_h
+#define header_h
+
+
+#endif /* header_h */
