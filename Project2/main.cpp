ca80cfd8aa70d8fae7936e32faf19a2aa4f9cc50
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-11-17 19:49:36 -0800

    Updated program with switch cases in the main file.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -24,12 +24,38 @@ int main() {
         cout << "Enter your choice: ";
         cin >> choice;
         
-        while (<#condition#>) {
-            <#statements#>
+        switch (choice) {
+            case 1:
+                addStock(stocks.get(), count);
+                break;
+            case 2:
+                displayPortfolio(stocks.get(), count);
+                break;
+            case 3:
+                if (count > 0) {
+                    for (int i = 0; i < count; ++i) {
+                        analyzeStock(stocks[i]);
+                    }
+                } else {
+                    cout << "No stocks to analyze.\n";
+                }
+                break;
+            case 4:
+                savePortfolio(stocks.get(), count);
+                break;
+            case 5:
+                loadPortfolio(stocks.get(), count);
+                break;
+            case 6:
+                searchStock(stocks.get(), count);
+                break;
+            case 0:
+                cout << "Exiting the program.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
         }
-        
-        
+    } while (choice != 0);
         
         return 0;
     }
-}
